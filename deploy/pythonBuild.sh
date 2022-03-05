558384aa5c8db0b6ca253d9b434e121dda38d387
“NDesassis” <nicolas.desassis@gmail.com>
2022-03-05 19:48:00 +0100

    Add deploy script

--- deploy/pythonBuild.sh
+++ deploy/pythonBuild.sh
@@ -4,12 +4,13 @@ export PATH=/opt/python/cp39-cp39/bin/:$PATH
 python3 -m pip install twine
 cmake -Bbuild 
 cmake --build build --target python_build
-cd build/python/Release
+cd $3
 python3 setup.py bdist_wheel --plat-name=manylinux_2_x86_64
 
 export TWINE_USERNAME= $1
 export TWINE_PASSWORD= $2
 
+echo $TWINE_USERNAME
 python3 -m twine upload --repository-url https://test.pypi.org/legacy/ $3/dist/*
 
 
