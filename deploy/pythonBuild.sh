df06777ed503880a958b3e998ff6a362598d7d2f
“NDesassis” <nicolas.desassis@gmail.com>
2022-03-05 23:40:39 +0100

    Add deploy script

--- deploy/pythonBuild.sh
+++ deploy/pythonBuild.sh
@@ -10,7 +10,7 @@ python3 setup.py bdist_wheel --plat-name=manylinux_2_x86_64
 export TWINE_USERNAME=$2
 export TWINE_PASSWORD=$3
 
-echo $1/dist/
+echo $1
 python3 -m twine upload --repository-url https://test.pypi.org/legacy/ $1/dist/*
 
 
