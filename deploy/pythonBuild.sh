8b0454269b2434e550321c7bd0dd1be1eb57c7b4
“NDesassis” <nicolas.desassis@gmail.com>
2022-03-06 11:49:32 +0100

    fix twine error

--- deploy/pythonBuild.sh
+++ deploy/pythonBuild.sh
@@ -7,8 +7,8 @@ cmake --build build --target python_build
 cd $1
 python3 setup.py bdist_wheel --plat-name=manylinux_2_17_x86_64
 
-
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/* -u ${{secrets.TEST_PIPY_API_LOGIN}} -p ${{secrets.TEST_PIPY_API_MDP}}
+echo $LOGIN
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/* -u $LOGIN -p $MDP
 
 
 
