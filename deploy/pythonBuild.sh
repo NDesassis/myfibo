85b481844570d784d3fdc54bddd561558681942f
“NDesassis” <nicolas.desassis@gmail.com>
2022-03-06 11:18:30 +0100

    fix twine error

--- deploy/pythonBuild.sh
+++ deploy/pythonBuild.sh
@@ -10,6 +10,6 @@ python3 setup.py bdist_wheel --plat-name=manylinux_2_x86_64
 export TWINE_USERNAME=$2
 export TWINE_PASSWORD=$3
 
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/*
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/* -u $TWINE_USERNAME -p $TWINE_PASSWORD
 
 
