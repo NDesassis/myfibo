767896443db7da74682392f85d5bcf5c038913ae
“NDesassis” <nicolas.desassis@gmail.com>
2022-03-06 11:26:20 +0100

    fix twine error

--- deploy/pythonBuild.sh
+++ deploy/pythonBuild.sh
@@ -5,7 +5,7 @@ python3 -m pip install twine
 cmake -Bbuild 
 cmake --build build --target python_build
 cd $1
-python3 setup.py bdist_wheel --plat-name=manylinux_2_x86_64
+python3 setup.py bdist_wheel --plat-name=manylinux_2_17_x86_64
 
 export TWINE_USERNAME=$2
 export TWINE_PASSWORD=$3
