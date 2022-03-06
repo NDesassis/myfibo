65ebffa5ea5d35068e0503618eb0064e32d123a3
“NDesassis” <nicolas.desassis@gmail.com>
2022-03-06 17:39:33 +0100

    fix twine error

--- deploy/pythonBuild.sh
+++ deploy/pythonBuild.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-export PATH=/opt/python/cp39-cp39/bin/:$PATH
-#python3 -m pip install twine
+if [ $2 = "cp38" ]; then
+	export PATH=/opt/python/cp38-cp38/bin/:$PATH
+fi
+
+if [ $2 = "cp39"]; then
+	export PATH=/opt/python/cp39-cp39/bin/:$PATH
+fi
+
+
 cmake -Bbuild 
 cmake --build build --target python_build
 cd $1
 python3 setup.py bdist_wheel --plat-name=manylinux_2_17_x86_64
-
-#python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --verbose dist/* -u $2 -p $3
 
 
 
